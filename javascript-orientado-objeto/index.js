1e07aefdbabbf9c2b18eec07aaddfb63d5ced516
MatheusShirakawa <matheus_shirakawa@outlook.com>
2022-01-17 00:54:30 -0300

    first commit

---
+++ javascript-orientado-objeto/index.js
@@ -0,0 +1,12 @@
+const cliente1Nome = "Ricardo";
+const cliente1Cpf = 11122233309;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Alice";
+const cliente2Cpf = 88822233309;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome);
+
