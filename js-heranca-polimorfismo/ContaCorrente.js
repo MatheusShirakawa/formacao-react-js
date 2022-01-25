afebbd14bb894ec4deedbc54fc3eaf440c627000
MatheusShirakawa <matheus_shirakawa@outlook.com>
2022-01-25 02:04:53 -0300

    creating the account class and using the extends and rewriting methods

--- js-heranca-polimorfismo/ContaCorrente.js
+++ js-heranca-polimorfismo/ContaCorrente.js
@@ -1,45 +1,16 @@
-import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-export class ContaCorrente{
+export class ContaCorrente extends Conta{
     static numero_contas = 0;
-
-    // assessores
-    set cliente(novoValor){
-        if(novoValor instanceof Cliente)
-            this._cliente = novoValor;
-    }
-
-    get cliente(){
-        return this._cliente;
-    }
-
-    get saldo(){
-        return this._saldo;
-    }
-
     // constructor
     constructor(agencia,cliente){
-        this.agencia = agencia;
-        this.cliente = cliente;
-        this._saldo = 0;
+        super(0, agencia,cliente);
         ContaCorrente.numero_contas += 1;
     }
 
-    // metodos
+    // sobrescrevendo o comportamento de sacar
     sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0) return;
-        this._saldo += valor;
-    }
-
-    transferir(valor, conta){
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
+        let taxa = 1.1;
+        return this._sacar(valor, taxa);
     }
 }
