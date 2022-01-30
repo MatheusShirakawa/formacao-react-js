54c6658e290a147ebf621febca3f059888709a00
MatheusShirakawa <matheus_shirakawa@outlook.com>
2022-01-30 03:52:55 -0300

    finishing the course

--- js-heranca-polimorfismo/index.js
+++ js-heranca-polimorfismo/index.js
@@ -1,18 +1,29 @@
 
 import { Cliente } from './Cliente.js';
-import { Conta } from './Conta.js';
-import { ContaCorrente } from './ContaCorrente.js';
-import { ContaPoupanca } from './ContaPoupanca.js';
+import { Conta } from './Conta/Conta.js';
+import { ContaCorrente } from './Conta/ContaCorrente.js';
+import { ContaPoupanca } from './Conta/ContaPoupanca.js';
+import { Gerente } from './Funcionario/Gerente.js';
+import { Diretor } from './Funcionario/Diretor.js';
+import { SistemaAutenticacao } from './SistemaAutenticacao.js';
 
-const cliente1 = new Cliente('Ricardo','003219312');
-const cliente2 = new Cliente('Teste','2313123');
+const cliente1 = new Cliente('Ricardo','003219312','123');
+// const cliente2 = new Cliente('Teste','2313123');
 
-const conta1 = new ContaCorrente(1001,cliente1);
-const conta2 = new ContaPoupanca(50,cliente2,1001);
+const diretor = new Diretor('Rodrigo', 10000, 2310391203);
+const gerente = new Gerente('Ricardo', 5000, 2301481293);
 
-const conta3 = new Conta(0,cliente2, 1002);
+diretor.cadastrarSenha("1234");
+gerente.cadastrarSenha("1234");
 
-// conta1.depositar(500);
+const gerenteEstaLogado = SistemaAutenticacao.login(diretor, "1234");
+const diretorEstaLogado = SistemaAutenticacao.login(gerente, "1234");
 
-console.log(conta3);
-// console.log(conta2);
+console.log(gerenteEstaLogado);
+console.log(diretorEstaLogado);
+
+
+// const conta1 = new ContaCorrente(1001,cliente1);
+// const conta2 = new ContaPoupanca(50,cliente2,1001);
+
+
