01b16dfd60b37e734aefae5fc77b75272ef407bb
MatheusShirakawa <matheus_shirakawa@outlook.com>
2022-02-14 11:15:15 -0300

    adding the categories and fix some issues

---
+++ js-ceep-part-two/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeCategorias from "./components/ListaDeCategorias";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      notas:[]
+    }
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <main className="conteudo-principal">
+          <ListaDeCategorias/>
+          <ListaDeNotas notas={this.state.notas}/>  
+        </main>
+      </section>
+    );
+  }
+}
+
+//new ListaDeNotas({notas:this.notas})
+export default App;
